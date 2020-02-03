e056faa5340404548805b08ca12c40fed921ad3e
stuffrome <gabeperez018@gmail.com>
2020-02-03 15:25:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jafra-backend
---
+++ src/main/java/com/senpro/jafrabackend/JafraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.senpro.jafrabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JafraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JafraBackendApplication.class, args);
+	}
+
+}
